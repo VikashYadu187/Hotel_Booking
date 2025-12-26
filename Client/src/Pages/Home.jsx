a2f7834d6808ca2157be2387665dded21d7ff2bd
vikashyadu187 <vikashyadu187@gmail.com>
2025-12-26 18:09:20 +0530

    initial commit

---
+++ Client/src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import FeaturedDestination from '../Components/FeaturedDestination'
+import ExclusiveOffer from '../Components/ExclusiveOffer'
+import Testimonial from '../Components/Testimonial'
+import NewsLetters from '../Components/NewsLetters'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeaturedDestination/>
+      <ExclusiveOffer/>
+      <Testimonial/>
+      <NewsLetters/>
+      
+    </>
+  )
+}
+
+export default Home
